1b4475df5ffe3b925167566de986a5d8eccafb2c
Yusa Islam <yusaislam48@gmail.com>
2021-04-11 09:58:26 +0600

    doctors portal home page done

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import HeaderMain from '../HeaderMain/HeaderMain';
+import Navbar from '../Navbar/Navbar';
+import BudinessInfo from '../BudinessInfo/BudinessInfo';
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <Navbar></Navbar>
+            <HeaderMain></HeaderMain>
+            <BudinessInfo></BudinessInfo>
+        </div>
+    );
+};
+
+export default Header;
